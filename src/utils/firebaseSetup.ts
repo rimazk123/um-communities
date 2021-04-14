6c43abb2005c63e767bc7f91efe98aed3e583ea4
Rimaz Khan <rimazk@umich.edu>
2021-04-14 18:55:21 -0400

    redo config

--- src/utils/firebaseSetup.ts
+++ src/utils/firebaseSetup.ts
@@ -1,5 +1,16 @@
 import firebase from "firebase";
-import config from "../firebaseConfig.json";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+console.log(config);
 
 firebase.initializeApp(config);
 
